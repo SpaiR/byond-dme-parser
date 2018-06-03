703adeccbd171bb89c8ffa899a84ef5f3dae56a5
SpaiR <klimbetoo@gmail.com>
2018-06-03 20:40:36 +0300

    Remove unquoting in VarUtil if value is `null`

--- src/main/java/io/github/spair/byond/VarUtil.java
+++ src/main/java/io/github/spair/byond/VarUtil.java
@@ -1,8 +1,7 @@
 package io.github.spair.byond;
 
+import java.util.Objects;
 import java.util.Optional;
-
-import static java.util.Objects.isNull;
 
 public final class VarUtil {
 
@@ -11,24 +10,28 @@ public final class VarUtil {
     }
 
     public static Optional<String> optionalUnquoted(final String var) {
-        if (isNull(var)) {
+        if (isEmptyVar(var)) {
             return Optional.empty();
         }
         return Optional.of(var.substring(1, var.length() - 1));
     }
 
     public static Optional<Integer> optionalInt(final String var) {
-        if (isNull(var)) {
+        if (isEmptyVar(var)) {
             return Optional.empty();
         }
         return Optional.of(Integer.parseInt(var));
     }
 
     public static Optional<Double> optionalDouble(final String var) {
-        if (isNull(var)) {
+        if (isEmptyVar(var)) {
             return Optional.empty();
         }
         return Optional.of(Double.parseDouble(var));
+    }
+
+    private static boolean isEmptyVar(final String var) {
+        return Objects.isNull(var) || ByondTypes.NULL.equals(var) || var.isEmpty();
     }
 
     private VarUtil() {
